76832788b477302bf8f93569bdcd543bfe7c89e2
DOUGLAS EMMANUEL <emmanueldouglas2121@gmail.com>
2024-10-31 14:00:31 +0000

    added 3 new screens

---
+++ lib/pages/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _Home();
+}
+
+class _Home extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:SafeArea(child: Text('Home Screen')),  //just like safeareaview in reactnative
+    );
+  }
+}
